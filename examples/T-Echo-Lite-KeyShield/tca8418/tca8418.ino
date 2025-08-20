abcf0b4727f19cba2479dc72c1dcfc990649ab0d
LILYGO_L <13073925292@163.com>
2025-08-20 15:52:30 +0800

    update tca8418

---
+++ examples/T-Echo-Lite-KeyShield/tca8418/tca8418.ino
@@ -0,0 +1,108 @@
+/*
+ * @Description: xl9535
+ * @Author: LILYGO_L
+ * @Date: 2025-06-13 14:20:16
+ * @LastEditTime: 2025-08-20 15:51:42
+ * @License: GPL 3.0
+ */
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_TinyUSB.h>
+#include "t_echo_lite_keyshield_config.h"
+#include "cpp_bus_driver_library.h"
+
+auto TCA8418_IIC_Bus = std::make_shared<Cpp_Bus_Driver::Hardware_Iic_2>(TCA8418_SDA, TCA8418_SCL, &Wire);
+
+auto TCA8418 = std::make_unique<Cpp_Bus_Driver::Tca8418>(TCA8418_IIC_Bus, TCA8418_IIC_ADDRESS, DEFAULT_CPP_BUS_DRIVER_VALUE);
+
+volatile bool Interrupt_Flag = false;
+
+void setup()
+{
+    Serial.begin(115200);
+    uint8_t serial_init_count = 0;
+    while (!Serial)
+    {
+        delay(100); // wait for native usb
+        serial_init_count++;
+        if (serial_init_count > 30)
+        {
+            break;
+        }
+    }
+    printf("Ciallo\n");
+
+    // 3.3V Power ON
+    pinMode(RT9080_EN, OUTPUT);
+    digitalWrite(RT9080_EN, HIGH);
+
+    // 必须加上电延时
+    delay(500);
+
+    attachInterrupt(TCA8418_INT, []() -> void
+                    { Interrupt_Flag = true; }, FALLING);
+
+    TCA8418->begin();
+    TCA8418->set_keypad_scan_window(0, 0, TCA8418_KEYPAD_SCAN_WIDTH, TCA8418_KEYPAD_SCAN_HEIGHT);
+    TCA8418->set_irq_pin_mode(Cpp_Bus_Driver::Tca8418::Irq_Mask::KEY_EVENTS);
+    TCA8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
+}
+
+void loop()
+{
+    // if (Interrupt_Flag == true)
+    // {
+    Cpp_Bus_Driver::Tca8418::Irq_Status is;
+
+    if (TCA8418->parse_irq_status(TCA8418->get_irq_flag(), is) == false)
+    {
+        printf("parse_irq_status fail\n");
+    }
+    else
+    {
+        if (is.key_events_flag == true)
+        {
+            Cpp_Bus_Driver::Tca8418::Touch_Point tp;
+            if (TCA8418->get_multiple_touch_point(tp) == true)
+            {
+                printf("touch finger: %d\n", tp.finger_count);
+
+                for (uint8_t i = 0; i < tp.info.size(); i++)
+                {
+                    switch (tp.info[i].event_type)
+                    {
+                    case Cpp_Bus_Driver::Tca8418::Event_Type::KEYPAD:
+                    {
+                        Cpp_Bus_Driver::Tca8418::Touch_Position tp_2;
+                        if (TCA8418->parse_touch_num(tp.info[i].num, tp_2) == true)
+                        {
+                            printf("keypad event\n");
+                            printf("   touch num:[%d] num: %d x: %d y: %d press_flag: %d\n", i + 1, tp.info[i].num, tp_2.x, tp_2.y, tp.info[i].press_flag);
+                            if (tp.info[i].num <= (sizeof(Tca8418_Map) / sizeof(std::string)))
+                            {
+                                printf("   touch string: %s\n", Tca8418_Map[tp.info[i].num - 1].c_str());
+                            }
+                        }
+
+                        break;
+                    }
+                    case Cpp_Bus_Driver::Tca8418::Event_Type::GPIO:
+                        printf("gpio event\n");
+                        printf("   touch num:[%d] num: %d press_flag: %d\n", i + 1, tp.info[i].num, tp.info[i].press_flag);
+                        break;
+
+                    default:
+                        break;
+                    }
+                }
+            }
+
+            TCA8418->clear_irq_flag(Cpp_Bus_Driver::Tca8418::Irq_Flag::KEY_EVENTS);
+        }
+    }
+
+    //     Interrupt_Flag = false;
+    // }
+
+    delay(10);
+}
--- libraries/Private_Library/t_echo_lite_keyshield_config.h
+++ libraries/Private_Library/t_echo_lite_keyshield_config.h
@@ -2,18 +2,38 @@
  * @Description: t_echo_lite_keyshield_config
  * @Author: LILYGO
  * @Date: 2024-12-06 14:37:43
- * @LastEditTime: 2025-08-19 18:04:57
+ * @LastEditTime: 2025-08-20 15:45:51
  * @License: GPL 3.0
  */
 #pragma once
 
 #include "t_echo_lite_config.h"
+#include <string>
 
 ////////////////////////////////////////////////// gpio config //////////////////////////////////////////////////
 
 // TCA8418
 #define TCA8418_SDA IIC_1_SCL
 #define TCA8418_SCL IIC_1_SDA
-#define TCA8418_INT
+#define TCA8418_INT EXT_2X5P_1_IO_1_3
 
 ////////////////////////////////////////////////// gpio config //////////////////////////////////////////////////
+
+////////////////////////////////////////////////// other define config //////////////////////////////////////////////////
+
+// TCA8418
+#define TCA8418_IIC_ADDRESS 0x34
+#define TCA8418_KEYPAD_SCAN_WIDTH 4
+#define TCA8418_KEYPAD_SCAN_HEIGHT 5
+// TCA8418键盘按键映射
+const std::string Tca8418_Map[] =
+    {
+        "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10",
+        "Esc", "Esc", "1", "2", "3", "4", "5", "6", "7", "8",
+        "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P",
+        "Caps", "A", "S", "D", "F", "G", "H", "J", "K", "L",
+        "Alt", "Z", "X", "C", "V", "B", "N", "M", "Ctrl", "Up",
+        "Fn", "Win", "Shift", "Tab", "Space", "Space", "Space", "Fn", "Left", "Down",
+        "F11", "9", "Del", "Enter", "Record", "Enter", "0", "Right"};
+
+////////////////////////////////////////////////// other define config //////////////////////////////////////////////////
