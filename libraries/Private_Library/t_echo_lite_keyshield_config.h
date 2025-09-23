227e6669f9d4a83cb4c73a33cd1aa47cb5c6163f
LILYGO_L <13073925292@163.com>
2025-09-23 12:10:02 +0800

    updata t_echo_lite_keyshield_config

--- libraries/Private_Library/t_echo_lite_keyshield_config.h
+++ libraries/Private_Library/t_echo_lite_keyshield_config.h
@@ -2,7 +2,7 @@
  * @Description: t_echo_lite_keyshield_config
  * @Author: LILYGO
  * @Date: 2024-12-06 14:37:43
- * @LastEditTime: 2025-09-12 15:40:01
+ * @LastEditTime: 2025-09-23 12:08:55
  * @License: GPL 3.0
  */
 #pragma once
@@ -13,13 +13,13 @@
 ////////////////////////////////////////////////// gpio config //////////////////////////////////////////////////
 
 // TCA8418
-#define TCA8418_SDA IIC_1_SCL
-#define TCA8418_SCL IIC_1_SDA
+#define TCA8418_SDA IIC_1_SDA
+#define TCA8418_SCL IIC_1_SCL
 #define TCA8418_INT EXT_2X5P_1_IO_1_3
 
 // ES8311
-#define ES8311_SDA IIC_1_SCL
-#define ES8311_SCL IIC_1_SDA
+#define ES8311_SDA IIC_1_SDA
+#define ES8311_SCL IIC_1_SCL
 #define ES8311_ADC_DATA EXT_2X5P_2_IO_0_23
 #define ES8311_DAC_DATA EXT_2X5P_2_IO_1_6
 #define ES8311_BCLK EXT_2X5P_2_IO_0_10
@@ -27,8 +27,8 @@
 #define ES8311_WS_LRCK EXT_2X5P_2_IO_0_25
 
 // AW86224
-#define AW86224_SDA IIC_1_SCL
-#define AW86224_SCL IIC_1_SDA
+#define AW86224_SDA IIC_1_SDA
+#define AW86224_SCL IIC_1_SCL
 
 // microphone
 #define MICROPHONE_SCLK EXT_2X5P_2_IO_0_9
